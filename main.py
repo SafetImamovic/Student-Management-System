de024be6f459ebb52c92f744d6e6938d1aeb54d7
Safet <safet.imamovic.22@size.ba>
2024-08-21 19:23:27 +0200

    Add Week 3 docs topic, Add API Versioning docs in the Week 3 topic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from app.database.database import SessionLocal, engine
 from app.database.database import Base
 from app.utils.seeding import seed
