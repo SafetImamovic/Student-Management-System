102b970aec717f8b7c2945356901ccae6da8c796
Safet <safet.imamovic.22@size.ba>
2024-08-23 18:27:43 +0200

    Modify README file to better instruct on how to set the project up, update .env configuration, create a function which checks if the database exists and if it doesn't creates it, update Alembic's env.py to match the configured .env, add .env.docker for docker specific env vars

---
+++ app/database/check_and_create_db.py
@@ -0,0 +1,55 @@
+from sqlalchemy import text, create_engine, Engine
+import sys
+
+
+def check_and_create_database(DB_USER: str, DB_PASS: str, DB_HOST: str, DB_PORT: str, DB_NAME: str) -> Engine:
+    """
+    This function creates the database if it does not exist.
+
+    It does that by creating a default engine (engine without a direct connection to a specific database).
+
+    Sets the isolation_level="AUTOCOMMIT" during it's instantiation.
+
+    PostgreSQL does not allow CREATE DATABASE to be executed within a transaction block.
+    This means we cannot use BEGIN and COMMIT or ROLLBACK for these operations.
+    The CREATE DATABASE command needs to be run immediately and independently of other
+    database changes.
+
+    It then checks the PSQL database catalogue (pg_database) for the correct database name.
+
+    If it does not exist, it creates it, then creates the Engine instance connecting to the
+    database and returns the Engine.
+
+    If it does exist, it does nothing.
+
+    Finally, it disposes of the default connection and returns the Engine.
+
+    :param DB_USER:
+    :param DB_PASS:
+    :param DB_HOST:
+    :param DB_PORT:
+    :param DB_NAME:
+    :return: Engine
+    """
+    default_database_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/"
+    default_engine = create_engine(default_database_url, isolation_level="AUTOCOMMIT")
+
+    try:
+        with default_engine.connect() as connection:
+            result = connection.execute(text(f"SELECT 1 FROM pg_database WHERE datname='{DB_NAME}';"))
+            exists = result.scalar() is not None
+
+            if not exists:
+                print(f"Database '{DB_NAME}' does not exist. Creating...")
+                connection.execute(text(f"CREATE DATABASE {DB_NAME};"))
+
+                print(f"Database '{DB_NAME}' created successfully.")
+            else:
+                print(f"Database '{DB_NAME}' already exists.")
+
+        return create_engine(default_database_url + DB_NAME)
+    except Exception as e:
+        print(f"An error occurred while checking or creating the database: {e}")
+        sys.exit(1)
+    finally:
+        default_engine.dispose(True)
--- app/database/database.py
+++ app/database/database.py
@@ -1,20 +1,23 @@
 import os
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 from dotenv import load_dotenv
+from .check_and_create_db import check_and_create_database
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQL_ALCHEMY_URL')
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_NAME")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine: Engine = check_and_create_database(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal: Session = sessionmaker(bind=engine)
 
-Base = declarative_base()
+Base: DeclarativeBase = declarative_base()
 
 
 # Dependency
