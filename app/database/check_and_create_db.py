8280c7b0b0e2574a0aab51c1ebaf6138480f20b5
Safet <safet.imamovic.22@size.ba>
2024-08-26 15:47:36 +0200

    Modifed the variables in check_and_create.py to use camel case

--- app/database/check_and_create_db.py
+++ app/database/check_and_create_db.py
@@ -3,7 +3,7 @@ import sys
 from sqlalchemy import text, create_engine, Engine
 
 
-def check_and_create_database(DB_USER: str, DB_PASS: str, DB_HOST: str, DB_PORT: str, DB_NAME: str) -> Engine:
+def check_and_create_database(db_user: str, db_pass: str, db_host: str, db_port: str, db_name: str) -> Engine:
     """
     This function creates the database if it does not exist.
 
@@ -25,35 +25,35 @@ def check_and_create_database(DB_USER: str, DB_PASS: str, DB_HOST: str, DB_PORT:
 
     Finally, it disposes of the default connection and returns the Engine.
 
-    :param DB_USER:
-    :param DB_PASS:
-    :param DB_HOST:
-    :param DB_PORT:
-    :param DB_NAME:
-    :return: Engine
+    :param db_user:
+    :param db_pass:
+    :param db_host:
+    :param db_port:
+    :param db_name:
+    :return:
     """
 
-    default_database_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/"
+    default_database_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/"
 
     default_engine = create_engine(default_database_url, isolation_level="AUTOCOMMIT")
 
     try:
         with default_engine.connect() as connection:
-            result = connection.execute(text(f"SELECT 1 FROM pg_database WHERE datname='{DB_NAME}';"))
+            result = connection.execute(text(f"SELECT 1 FROM pg_database WHERE datname='{db_name}';"))
 
             exists = result.scalar() is not None
 
             if not exists:
-                print(f"Database '{DB_NAME}' does not exist. Creating...")
+                print(f"Database '{db_name}' does not exist. Creating...")
 
-                connection.execute(text(f"CREATE DATABASE {DB_NAME};"))
+                connection.execute(text(f"CREATE DATABASE {db_name};"))
 
-                print(f"Database '{DB_NAME}' created successfully.")
+                print(f"Database '{db_name}' created successfully.")
 
             else:
-                print(f"Database '{DB_NAME}' already exists.")
+                print(f"Database '{db_name}' already exists.")
 
-        return create_engine(default_database_url + DB_NAME)
+        return create_engine(default_database_url + db_name)
 
     except Exception as e:
         print(f"An error occurred while checking or creating the database: {e}")
